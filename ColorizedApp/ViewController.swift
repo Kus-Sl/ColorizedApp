3c9fd77a9961fcfe9725f7f0ec98c17e4e501838
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-04-13 03:59:07 +0300

    Task ColorizedApp is complete.

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+
+    @IBOutlet weak var redColorValue: UILabel!
+    @IBOutlet weak var greenColorValue: UILabel!
+    @IBOutlet weak var blueColorValue: UILabel!
+
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        colorView.layer.cornerRadius = 20
+
+        redSlider.minimumTrackTintColor = UIColor.red
+        greenSlider.minimumTrackTintColor = UIColor.green
+        blueSlider.minimumTrackTintColor = UIColor.blue
+
+        changeColorValue()
     }
 
+    @IBAction func changeColorView() {
+        changeColor()
+        changeColorValue()
+    }
+
+    private func changeColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+
+    private func changeColorValue() {
+        redColorValue.text = String(format: "%.2f", redSlider.value)
+        greenColorValue.text = String(format: "%.2f", greenSlider.value)
+        blueColorValue.text = String(format: "%.2f",blueSlider.value)
+    }
 
 }
-
