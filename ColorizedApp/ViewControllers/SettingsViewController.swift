8935bf406c4cf2256bf3fdc1acbe0b4fcd7bd614
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-05-03 12:51:49 +0300

    SettingsViewController: - removing textFieldShouldReturn - adding isValidNumber method - adding showAlert method

--- ColorizedApp/ViewControllers/SettingsViewController.swift
+++ ColorizedApp/ViewControllers/SettingsViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    //сделать проверку на черный цвет в мейн экране
+
     @IBOutlet weak var colorView: UIView! {
         didSet { colorView.layer.cornerRadius = 20 }
     }
@@ -60,6 +62,7 @@ class SettingsViewController: UIViewController {
 
 // MARK: Sliders settings
 extension SettingsViewController {
+
     private func setColorValues(accordingTo colors: [CGFloat]) {
         for (color, slider) in zip(colors, rgbSliders) {
             slider.value = Float(color)
@@ -81,9 +84,9 @@ extension SettingsViewController {
 
     private func setTextFieldValue(for tag: Int, from textField: UITextField) {
         colorValuesLabels[tag].text = textField.text
-        if let test = textField.text {
-            if let testt = Float(test) {
-                rgbSliders[tag].value = testt
+        if let text = textField.text {
+            if let text = Float(text) {
+                rgbSliders[tag].value = text
             }
         }
     }
@@ -103,18 +106,16 @@ extension SettingsViewController {
 
 // MARK: KeyBoard's methods
 extension SettingsViewController: UITextFieldDelegate {
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        setTextFieldValue(for: textField.tag, from: textField)
-        setViewColor()
-    }
-
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.endEditing(true)
-    }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super .touchesBegan(touches, with: event)
         view.endEditing(true)
+    }
+
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard isValidValue(value: textField) else { return }
+        setTextFieldValue(for: textField.tag, from: textField)
+        setViewColor()
     }
 
     func textField(
@@ -124,21 +125,54 @@ extension SettingsViewController: UITextFieldDelegate {
     ) -> Bool {
 
         replaceСommaToDot(for: string, in: textField)
+        return true
     }
 
-
-    private func replaceСommaToDot(
-        for string: String,
-        in textField: UITextField
-    ) -> Bool {
-
+    private func replaceСommaToDot(for string: String, in tf: UITextField) {
         if string == "," {
-            guard let text = textField.text else { return true }
-            textField.text = text + "."
-            return false
+            guard let text = tf.text else { return }
+            tf.text = text + "."
+        }
+    }
+}
+
+// MARK: Value validation
+extension SettingsViewController {
+
+    private func isValidValue(value: UITextField) -> Bool {
+        guard let validatingString = value.text else { return false }
+        if let validatingNumber = Double(validatingString) {
+            if validatingNumber.description.count <= 4 {
+                if validatingNumber >= 0 && validatingNumber <= 1.0 {
+                } else {
+                    showAlert(title: "Ошибка",
+                              message: "Введите значение от 0.00 до 1.00",
+                              for: value)
+                    return false
+                }
+            }
         }
         return true
     }
 
+    private func showAlert(
+        title: String,
+        message: String,
+        for textField: UITextField
+    ) {
+
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+
+        let alertAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField.text = nil
+        }
+
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
 }
 
